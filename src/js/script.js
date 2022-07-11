3ff8ab6ce0b76117de9fe08eb0f5d3af8c48ef2e
Oleg <oleg.barskiy17@gmail.com>
2022-07-11 14:58:08 +0300

    shashe_nado.zzz

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-  $('.carousel__inner').slick({
-    speed: 1200,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        setting: {
-           dots: true,
-           arrows:false
-        }
-      }
-    ]
-  });
+const slider = tns({
+  container: '.carousel__inner',
+  controls: false,
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  nav: false,
+  mouseDrag: true
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
